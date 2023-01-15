401f09a7e4c128d14e4c9b50a3b74ab922fb3ec0
Maida <maida.maxime@gmail.com>
2023-01-15 20:11:37 +0100

    Add token for auth

--- src/pages/Adherent.jsx
+++ src/pages/Adherent.jsx
@@ -5,32 +5,49 @@ export default function Adherent() {
   const [password, setPassword] = useState("");
   const [isCorrect, setIsCorrectPassword] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isValidCheck, setIsValidCheck] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (password == myPassword) {
+      localStorage.setItem(
+        "access_token",
+        "3174A6F92C489A0FA457366B0C7D17565D66072D81AE89E612888CD053553ACE"
+      );
       setIsCorrectPassword(true);
       setIsSubmitted(false);
+      setIsValidCheck(true);
     } else {
       setIsSubmitted(true);
     }
   };
 
+  const checkToken = () => {
+    return localStorage.getItem("access_token");
+  };
+
   return (
     <div className="adherent">
-      <h1>Adherent</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Mot de passe :{" "}
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-        <button type="submit">Soumettre</button>
-      </form>
-      {isCorrect ? <p>Contenu protégé</p> : null}
+      <h1>Adhérent</h1>
+      {!isValidCheck && !checkToken() ? (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Mot de passe :
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
+          <button type="submit">Soumettre</button>
+        </form>
+      ) : null}
+
+      {isCorrect ||
+      checkToken() ===
+        "3174A6F92C489A0FA457366B0C7D17565D66072D81AE89E612888CD053553ACE" ? (
+        <p>Contenu protégé</p>
+      ) : null}
       {isSubmitted ? (
         <p style={{ color: "red" }}>Mauvais mot de passe</p>
       ) : null}
